8558d524ed9fd6ebdf7dc5b54738cad15f2ad130
Alisha Aggarwal <101794538+48alisha@users.noreply.github.com>
2025-09-01 21:05:05 +0530

    Time: 0 ms (100%), Space: 41.5 MB (74.92%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,24 +1,33 @@
 class Solution {
     public int countHillValley(int[] nums) {
-     int i=1;
-     int j=i-1;
-     int k=i+1;
-     int count=0;
-     for(;k<nums.length;){
-        while(k<nums.length && nums[i]==nums[k]){
-            k++;
-        }
-        if(k<nums.length && nums[i]!=nums[j] && nums[i]!=nums[k]){
-            if((nums[i]>nums[j] && nums[i]>nums[k]) ||(nums[i]<nums[j] && nums[i]<nums[k])){
-              count++;
-            }
-        }
-        i=k;
-        j=i-1;
-        k=i+1;
+        int i = 1;
+        int j = i - 1;
+        int k = i + 1;
+        int count = 0;
 
-     }
-     return count;
-        
+        for (; k < nums.length; ) {
+            // skip duplicates on the left
+            while (j >= 0 && nums[i] == nums[j]) {
+                j--;
+            }
+
+            // skip duplicates on the right
+            while (k < nums.length && nums[i] == nums[k]) {
+                k++;
+            }
+
+            if (j >= 0 && k < nums.length) {
+                if ((nums[i] > nums[j] && nums[i] > nums[k]) ||
+                    (nums[i] < nums[j] && nums[i] < nums[k])) {
+                    count++;
+                }
+            }
+
+            // move window
+            i = k;
+            j = i - 1;
+            k = i + 1;
+        }
+        return count;
     }
 }
