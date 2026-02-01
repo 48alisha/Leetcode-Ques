35afb9a693af1d475ef2edbb75b166c1c32fa619
Alisha Aggarwal <101794538+48alisha@users.noreply.github.com>
2026-02-01 15:06:20 +0530

    Time: 3 ms (99.26%), Space: 175.4 MB (70.2%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,35 +1,21 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        ArrayList<Integer> arr3 = new ArrayList<>();
+        int ans[] = new int[nums.length];
+        int pos = 0;
+        int neg = 1;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                arr1.add(nums[i]);
+            if (nums[i] > 0) {
+                ans[pos]=nums[i];
+                pos+=2;
 
-            } else {
-                arr2.add(nums[i]);
             }
-
+            else{
+                ans[neg]=nums[i];
+                neg+=2;
+            }
         }
-        for(int i=0;i<arr2.size();i++){
-            arr3.add(arr2.get(i));
-            arr3.add(arr1.get(i));
-        }
-
-        int num[]=new int [arr3.size()];
-        int j=0;
-
-        for(int i=0;i<arr3.size();i++){
-            num[j]=arr3.get(i);
-            j++;
-
-        }
-        return num;
-
-
+        return ans;
 
     }
 }
