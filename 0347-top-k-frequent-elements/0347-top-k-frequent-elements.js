edf19dceace839b2e62dba102b25d51f1c5375f0
Alisha Aggarwal <101794538+48alisha@users.noreply.github.com>
2026-04-16 23:24:51 +0530

    Time: 10 ms (76.16%), Space: 60.3 MB (51.65%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map=new Map();
+    for(let num of nums){
+        map.set(num,(map.get(num)||0)+1)
+    }
+    let arr=Array.from(map.entries());
+    arr.sort((a,b)=>b[1]-a[1]);
+
+    return arr.slice(0,k).map(x=>x[0]);
+    
+};
