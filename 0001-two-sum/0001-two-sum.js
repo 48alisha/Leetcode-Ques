e784008058288163edd618467cd86748a0438cc1
Alisha Aggarwal <101794538+48alisha@users.noreply.github.com>
2026-04-17 09:43:10 +0530

    Time: 1 ms (85.93%), Space: 54.2 MB (81.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map={};
-    for(let i=0;i<nums.length;i++){
-        let diff=target-nums[i]
-        if(map[diff]!==undefined){
-            return [map[diff],i];
-        }
-        map[nums[i]]=i;
+  let map={};
+  for(let i=0;i<nums.length;i++){
+    let diff=target-nums[i];
+    if(map[diff]!==undefined){
+        return [map[diff],i];
     }
+    map[nums[i]]=i;
+  }
     
 };
