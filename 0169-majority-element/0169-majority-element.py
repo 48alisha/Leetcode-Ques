586ff6341f2d9f201117bdb99b1e3f466e98c563
Alisha Aggarwal <101794538+48alisha@users.noreply.github.com>
2026-06-11 13:15:18 +0530

    Time: 4 ms (79.55%), Space: 13.6 MB (40.57%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        freq = {}
+
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+
+        for key, value in freq.items():
+            if value > len(nums) // 2:
+                return key
